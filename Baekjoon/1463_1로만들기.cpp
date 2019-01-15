8631781d4eba00670fc2b185f6eb660fb9cb1f57
wh2per <93negaro@gmail.com>
2019-01-16 04:28:09 +0900

    1463_1로만들기

---
+++ Baekjoon/1463_1로만들기.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int d[1000001];
+
+int bj_1463() {
+	int n;
+	cin >> n;
+	for (int i = 2; i <= n; i++) {
+		d[i] = d[i - 1] + 1;
+		if (i % 2 == 0 && d[i] > d[i / 2] + 1)
+			d[i] = d[i / 2] + 1;
+		if (i % 3 == 0 && d[i] > d[i / 3] + 1)
+			d[i] = d[i / 3] + 1;
+	}
+
+	cout << d[n];
+
+	return 0;
+}
