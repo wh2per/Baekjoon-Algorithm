502609f53764f0f56e9fde7b7bc8c460cee6472f
wh2per <93negaro@gmail.com>
2019-01-17 09:23:51 +0900

    1676_팩토리얼0의개수

---
+++ Baekjoon/1676_팩토리얼0의개수.java
@@ -0,0 +1,13 @@
+/*
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+			Scanner scan = new Scanner(System.in);
+			int num = scan.nextInt();
+			int count = num/5;
+			int count2 = num/25;
+			int count3 = num/125;
+			System.out.println(count+count2+count3);
+	}
+}
+*/
