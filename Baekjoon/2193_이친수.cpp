038c405cd14f616fa1569944342ddc0c0957e13e
wh2per <93negaro@gmail.com>
2019-01-18 04:29:43 +0900

    2193_이친수

---
+++ Baekjoon/2193_이친수.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+long long d[91][2];
+
+int bj_2193() {
+	int n;
+	cin >> n;
+
+	d[1][0] = 0;
+	d[1][1] = 1;
+
+	for (int i = 2; i <= n; i++) {
+		d[i][0] = d[i - 1][0] + d[i - 1][1];
+		d[i][1] = d[i - 1][0];
+	}
+
+
+	long long ans = 0;
+	ans = d[n][0] + d[n][1];
+
+	cout << ans;
+
+	return 0;
+}
