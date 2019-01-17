4ad89325cec94e9c29b8ccf0207020b6fdfcb3ce
wh2per <93negaro@gmail.com>
2019-01-17 09:23:40 +0900

    1476_날짜계산

---
+++ Baekjoon/1476_날짜계산.java
@@ -0,0 +1,32 @@
+/*
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int E = scan.nextInt();
+		int S = scan.nextInt();
+		int M = scan.nextInt();
+		int te = 0;
+		int ts = 0;
+		int tm = 0;
+		int count = 0;
+
+		while (te != E || ts != S || tm != M) {
+			te++;
+			ts++;
+			tm++;
+			count++;
+			if (te > 15)
+				te = 1;
+			if (ts > 28)
+				ts = 1;
+			if (tm > 19)
+				tm = 1;
+		}
+
+		System.out.println(count);
+	}
+}
+*/
