1f89252912807f1f3334a50a19109c7b8a9c17cc
wh2per <93negaro@gmail.com>
2019-01-19 11:43:10 +0900

    2438_별찍기-1

---
+++ Baekjoon/2438_일곱난쟁이.java
@@ -0,0 +1,18 @@
+/*
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n= scan.nextInt();
+		
+		for(int j=0; j<n; j++) {
+			for(int i=0; i<=j; i++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+*/
