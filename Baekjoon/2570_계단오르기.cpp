d847fe11617c3d263942651d5581b2f188f05ed5
wh2per <93negaro@gmail.com>
2019-01-19 13:40:24 +0900

    2579_계단오르기

---
+++ Baekjoon/2570_계단오르기.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int bj_2570() {
+	int n;
+	cin >> n;
+	int a[301];
+	int d[301][2];
+
+	for (int i = 0; i < n; i++)
+		cin >> a[i + 1];
+
+	d[1][1] = a[1];
+
+	for (int i = 2; i <= n; i++) {
+		d[i][2] = d[i - 1][1] + a[i];
+		d[i][1] = max(d[i - 2][1], d[i - 2][2]) + a[i];
+	}
+
+	cout << max(d[n][1], d[n][2]);
+
+	return 0;
+}
