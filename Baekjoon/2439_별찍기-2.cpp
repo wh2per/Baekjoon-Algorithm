5413ad7c102f51ccbf348d02ef5ed36e1b9714d2
wh2per <93negaro@gmail.com>
2019-01-19 11:48:08 +0900

    2439_별찍기-3

--- Baekjoon/2439_별찍기-2.cpp
+++ Baekjoon/2439_별찍기-2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int bj_2439() {
 	int x;
 	cin >> x;
 
--- Baekjoon/2440_별찍기-3.cpp
+++ Baekjoon/2440_별찍기-3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int bj_2440() {
 	int x;
 	cin >> x;
 
