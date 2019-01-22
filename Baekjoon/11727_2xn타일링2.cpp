bb0f5e8534b1baa87db5ee107f129d4bc40dc3c2
wh2per <93negaro@gmail.com>
2019-01-23 00:57:48 +0900

    11727_2xn타일링2

---
+++ Baekjoon/11727_2xn타일링2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int d[1001];
+
+int bj_11727() {
+	int n;
+	cin >> n;
+
+	d[0] = 1;
+	d[1] = 1;
+
+	if (n > 1) {
+		for (int i = 2; i <= n; i++) {
+			d[i] = (2 * d[i - 2] + d[i - 1]) % 10007;
+		}
+
+		cout << d[n];
+	}
+	else
+		cout << 1;
+	return 0;
+}
