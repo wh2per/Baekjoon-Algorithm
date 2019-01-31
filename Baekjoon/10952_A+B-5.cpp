c3596dfca0dbd4e98b4161373a5838992e5d6777
wh2per <93negaro@gmail.com>
2019-02-01 03:43:33 +0900

    10952_A+B-5

---
+++ Baekjoon/10952_A+B-5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_10952() {
+	while(1){
+		int a, b;
+		cin >> a >> b;
+
+		if (a == 0 && b == 0)
+			break;
+
+		cout << a + b << endl;
+	} 
+
+	return 0;
+}
