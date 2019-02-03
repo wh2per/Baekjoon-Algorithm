049aaa297ded8f6f3d884f5ab48b85b9b08ce5d4
wh2per <93negaro@gmail.com>
2019-02-04 03:51:36 +0900

    10817_세수

---
+++ Baekjoon/10817_세수.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int bj_10817() {
+	vector<int> v(3);
+	for (int i = 0; i < 3; i++)
+		cin >> v[i];
+	sort(v.begin(), v.end());
+	cout << v[1];
+	return 0;
+}
