54f6f5483e25a58ff6fc03772a4958a6f67bdfc8
wh2per <93negaro@gmail.com>
2019-02-06 04:30:24 +0900

    1924_2007년

---
+++ Baekjoon/1924_2007년.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int bj_1924() {
+	vector<int> dates = {31,28,31,30,31,30,31,31,30,31,30,31};
+	vector<string> day = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
+	int x, y;
+	cin >> x >> y;
+	int count=0;
+	for (int i = 1; i < x; i++)
+		count += dates[i - 1];
+	count += y;
+	cout << day[count % 7];
+
+	return 0;
+}
