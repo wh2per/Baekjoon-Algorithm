6639ac1b4cad153bf742e94692d5a6e452d4eeed
wh2per <93negaro@gmail.com>
2019-02-18 05:54:57 +0900

    4344_평균은넘겠지

---
+++ Baekjoon/4344_평균은넘겠지.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int bj_4344(){
+	int C;
+	cin >> C;
+	for (int i = 0; i < C; i++) {
+		int N;
+		cin >> N;
+		float score = 0;
+		float cnt = 0;
+		vector<int> scores(N);
+		for (int j = 0; j < N; j++) {
+			cin >> scores[j];
+			score += scores[j];
+		}
+		score = score / N;
+		for (int k = 0; k < N; k++) {
+			if (scores[k] > score)
+				cnt++;
+		}
+		cout << fixed;
+		cout.precision(3);
+		cout << cnt * 100 /N << "%"<<endl;
+	}
+	return 0;
+}
