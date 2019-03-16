9514b27dec28af05b1ea8a47216dbaa60c54cccb
wh2per <93negaro@gmail.com>
2019-03-17 03:10:18 +0900

    2739_구구단

---
+++ Baekjoon/2739_구구단.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_2739() {
+	int N; 
+	cin >> N;
+
+	for (int i = 1; i < 10; i++)
+		cout << N << " * " << i << " = " << N * i << endl;
+	return 0;
+}
