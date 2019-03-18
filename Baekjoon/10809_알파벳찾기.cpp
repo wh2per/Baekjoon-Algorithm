c011cc6e871bab3806e29bd2a2710b274e6a5fe1
wh2per <93negaro@gmail.com>
2019-03-19 08:48:11 +0900

    10809_알파벳찾기

---
+++ Baekjoon/10809_알파벳찾기.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int bj_10809() {
+	string s;
+	cin >> s;
+
+	for (int i = 0; i < 26; i++) {
+		int temp = s.find('a' + i);
+		cout << temp << " ";
+	}
+
+	return 0;
+}
