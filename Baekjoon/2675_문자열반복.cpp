746758d21aa27ed11a5cfbe0656ea7f9cfc1631f
wh2per <93negaro@gmail.com>
2019-03-19 08:56:36 +0900

    2675_문자열반복

---
+++ Baekjoon/2675_문자열반복.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int bj_2675() {
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int R;
+		cin >> R;
+		string s;
+		cin >> s;
+
+		string ans="";
+		for (int j = 0; j < s.length(); j++) {
+			for (int k = 0; k < R; k++)
+				ans += s[j];
+		}
+		cout << ans << endl;
+	}
+	
+	return 0;
+}
