1ca258704f387443426b8d580e24ae812d41dc75
wh2per <93negaro@gmail.com>
2019-03-20 15:03:46 +0900

    11365_!밀비급일

---
+++ Baekjoon/11365_!밀비급일.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int bj_11365() {
+	string str="";
+	vector<string> v;
+
+	while (str != "END") {
+		getline(cin, str);
+		v.push_back(str);
+	}
+	
+	for (int i = 0; i < v.size() - 1; i++) {
+		for (int j = v[i].length()-1; j >= 0; j--) {
+			cout << v[i][j];
+		}
+		cout << '\n';
+	}
+	return 0;
+}
