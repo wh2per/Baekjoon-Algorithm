04685fd1c90168d275e4ea5da4fec27397ee7bce
wh2per <93negaro@gmail.com>
2019-03-21 09:42:05 +0900

    2941_크로아티아알파벳

---
+++ Baekjoon/2941_크로아티아알파벳.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>	
+using namespace std;
+
+int main() {
+	vector<string> v = { "c=","c-","dz=","d-","lj","nj","s=","z="};
+	string s;
+	cin >> s;
+	int size = s.length();
+
+	for (int i = 0; i < size; i++) {
+		for (string c : v) {
+			if (s.find(c) != string::npos) {
+				int index = 0;
+				index = s.find(c);
+				s.erase(index, c.length());
+				s.insert(index, "#");
+			}
+		}
+	}
+	cout << s.length();
+	return 0;
+}
