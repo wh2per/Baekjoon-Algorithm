9d581411c8c559294379e9d8f7ef16b3cdecffb7
wh2per <93negaro@gmail.com>
2019-03-21 09:12:10 +0900

    2902_KMP는왜KMP일까

---
+++ Baekjoon/2902_KMP는왜KMP일까.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int bj_2902() {
+	string s;
+	string ans = "";
+	cin >> s;
+
+	for (char c : s) {
+		if (isupper(c))
+			ans += c;
+	}
+	cout << ans;
+	return 0;
+}
