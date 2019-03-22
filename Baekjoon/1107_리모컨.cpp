9f4e9c222e3ae46b09751a24b1ac17b3063995ef
wh2per <93negaro@gmail.com>
2019-03-22 18:39:53 +0900

    1107_리모컨

---
+++ Baekjoon/1107_리모컨.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+bool broken[10];
+int possible(int c) {
+	if (c == 0) {
+		if (broken[0])
+			return 0;
+		else
+			return 1;
+	}
+	int len = 0;
+	while (c > 0) {
+		if (broken[c % 10])
+			return 0;
+		len += 1;
+		c /= 10;
+	}
+	return len;
+}
+int bj_1107() {
+	int n;
+	cin >> n;
+	int m;
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		int x;
+		cin >> x;
+		broken[x] = true;
+	}
+	int ans = abs(n - 100);		// +,- 로 가는 횟수 
+
+
+	for (int i = 0; i <= 1000000; i++) {
+		int c = i;
+		int len = possible(c);
+		if (len > 0) {
+			int press = abs(c - n);
+			if (ans > len + press)
+				ans = len + press;
+		}
+	}
+	cout << ans;
+	return 0;
+}
