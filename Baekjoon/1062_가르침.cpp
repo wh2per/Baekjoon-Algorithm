d252405ab3b7897c1e367bcc5179da7a6bccc71d
wh2per <93negaro@gmail.com>
2019-03-23 00:10:07 +0900

    14391_종이조각

---
+++ Baekjoon/1062_가르침.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int ans = 0;
+	set<char> haveto;
+	set<char> study;
+	study.insert('a');
+	study.insert('n');
+	study.insert('t');
+	study.insert('c');
+	study.insert('i');
+
+	int N, M;
+	cin >> N >> M;
+	
+	vector<set<char>> v;
+	for (int i = 0; i < N; i++) {
+		string s;
+		cin >> s;
+		s = s.substr(4, s.length()-8);
+		set<char> us;
+		for (int j = 0; j < s.length(); j++) {
+			if (study.count(s[j]) == 0) { 		// 안 배운 단어
+				us.insert(s[j]);
+				haveto.insert(s[j]);
+			}
+		}
+		v.push_back(us);			// 각 단어별로 배움이 필요한 글자를 들고있음
+	}
+	
+	if (M < 5) {
+		cout << 0;
+		return 0;
+	}
+
+	if (M == 26) {
+		cout << N;
+		return 0;
+	}
+
+	int size = haveto.size();
+	vector<char> temp;
+	vector<int> check(size, 1);
+
+	for (char c : haveto)
+		temp.push_back(c);
+
+	for (int i = 0; i < M - 5; i++)
+		check[i] = 0;
+
+	do {
+		int tans = 0;
+		set<char> s;		// 배운 단어
+		for (int i = 0; i < size; i++) {
+			if (check[i] == 0)
+				s.insert(temp[i]);
+		}
+
+		for (int i = 0; i < v.size(); i++) {
+			int cnt = 0;
+			for (char c : v[i]) {
+				if (s.count(c) != 0)		// 필요한 단어라면
+					cnt++;
+			}
+			if (cnt == v[i].size())
+				tans++;
+		}
+		if (tans > ans)
+			ans = tans;
+	} while (next_permutation(check.begin(), check.end()));
+
+	cout << ans;
+	
+	return 0;
+}
---
+++ Baekjoon/14391_종이조각.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int N, M;
+int ans_paper;
+vector<vector<int>> check_paper;
+vector<vector<int>> map_paper;
+
+void dfs_paper(int n, int m) {
+	if (n==N-1 && m==M-1) {		// 종료
+		vector<vector<bool>> visit(N,vector<bool>(M,false));
+		int ans_temp=0;
+		for (int a = 0; a < N; a++) {
+			for (int b = 0; b < M; b++) {
+				if (!visit[a][b]) {
+					int temp = 0;
+					if (check_paper[a][b] == 0) {		// 가로
+						int tb = b;
+						while (tb<M && visit[a][tb] == false && check_paper[a][tb] == 0) {
+							visit[a][tb] = true;
+							temp *= 10;
+							temp += map_paper[a][tb];
+							tb++;
+						}
+					}
+					else {						// 세로
+						int ta = a;			
+						while (ta<N && visit[ta][b] == false && check_paper[ta][b] == 1) {
+							visit[ta][b] = true;
+							temp *= 10;
+							temp += map_paper[ta][b];
+							ta++;
+						}
+					}
+					ans_temp += temp;
+				}
+			}
+		}
+		if (ans_temp > ans_paper)
+			ans_paper = ans_temp;
+
+		return;
+	}
+	if (m < M-1) {
+		m++;
+		check_paper[n][m] = 0;
+		dfs_paper(n, m);		// 0으로 선택
+		check_paper[n][m] = 1;
+		dfs_paper(n, m);		// 1로 선택
+	}
+	else {
+		n++;
+		m = 0;
+		check_paper[n][m] = 0;
+		dfs_paper(n, m);		// 0으로 선택
+		check_paper[n][m] = 1;
+		dfs_paper(n, m);		// 1로 선택
+	}
+}
+
+int bj_14391() {
+	ans_paper = INT_MIN;
+	cin >> N >> M;
+	map_paper.resize(N, vector<int>(M, -1));
+	check_paper.resize(N, vector<int>(M, -1));
+
+	for (int i = 0; i < N; i++) {
+		string s;
+		cin >> s;
+		for (int j = 0; j < M; j++) {
+			map_paper[i][j] = s[j]-'0';
+		}
+	}
+
+	check_paper[0][0] = 0;
+	dfs_paper(0, 0);
+	check_paper[0][0] = 1;
+	dfs_paper(0, 0);
+
+	cout << ans_paper;
+	return 0;
+}
