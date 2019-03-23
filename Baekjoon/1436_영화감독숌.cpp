8a9a21408606addbbeb745241d112eb81a8e1698
wh2per <93negaro@gmail.com>
2019-03-24 01:19:52 +0900

    1436_영화감독숌

---
+++ Baekjoon/1436_영화감독숌.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int bj_1436() {
+	int N;
+	cin >> N;
+	
+	int ans = 0;
+	int count = 0;
+
+	for (int i = 666; ; i++) {
+		string s = to_string(i);
+		if (s.find("666") != -1)	// 못찾았으면
+			count++;
+		if (count == N) {
+			cout << i;
+			break;
+		}
+	}
+	return 0;
+}
