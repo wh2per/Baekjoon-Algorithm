820692e3dc118bb4dc4ccd3b342a518cf090d29c
wh2per <93negaro@gmail.com>
2019-03-26 01:13:06 +0900

    2751_수정렬하기2

---
+++ Baekjoon/2751_수정렬하기2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int bj_2751() {
+	int N;
+	cin >> N;
+	vector<int> v(N);
+	for (int i = 0; i < N; i++)
+		cin >> v[i];
+
+	sort(v.begin(), v.end(),less<int>());
+
+	for (auto i : v)
+		cout << i << '\n';
+	return 0;
+}
