609c4f4cd620318c3ae29cceac2494ae0e5a4ae6
wh2per <93negaro@gmail.com>
2019-03-26 01:23:28 +0900

    10989_수정렬하기3

---
+++ Baekjoon/10989_수정렬하기3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int bj_10989() {
+	ios_base::sync_with_stdio(false);  
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	int N;
+	cin >> N;
+	vector<int> v(10001,0);
+	for (int i = 0; i < N; i++) {
+		int x;
+		cin >> x;
+		++v[x];
+	}
+
+	for (int i = 1; i < 10001; i++) {
+		if (v[i] > 0) {
+			for(int j=0; j<v[i]; j++)
+				cout << i << '\n';
+		}
+	}
+	return 0;
+}
