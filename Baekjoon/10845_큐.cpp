d7c6fca15dfe329c22b4c12e386d9549a9b7ea38
wh2per <93negaro@gmail.com>
2019-03-26 19:01:32 +0900

    10845_큐

---
+++ Baekjoon/10845_큐.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <queue>
+using namespace std;
+
+int bj_10845() {
+	queue<int> q;
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		string str;
+		cin >> str;
+		if (str == "push") {
+			int num;
+			cin >> num;
+			q.push(num);
+		}
+		else if (str == "front") {
+			if (q.empty())
+				cout << -1 << '\n';
+			else
+				cout << q.front() << '\n';
+		}
+		else if (str == "back") {
+			if (q.empty())
+				cout << -1 << '\n';
+			else
+				cout << q.back() << '\n';
+		}
+		else if (str == "pop") {
+			if (q.empty())
+				cout << -1 << '\n';
+			else {
+				cout << q.front() << '\n';
+				q.pop();
+			}
+		}
+		else if (str == "size") {
+			cout << q.size() << '\n';
+		}
+		else if (str == "empty") {
+			if (q.empty())
+				cout << 1 << '\n';
+			else
+				cout << 0 << '\n';
+		}
+	}
+	return 0;
+}
