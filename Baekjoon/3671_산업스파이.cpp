e175871b8b452e31e6c6c08f6eb0b5d3cca3fadf
wh2per <93negaro@gmail.com>
2019-03-30 01:44:38 +0900

    3671_산업스파이

---
+++ Baekjoon/3671_산업스파이.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+bool check_spy(int num) {
+	int i;
+	for (i = 2; i*i < num; ++i) {
+		if (num%i == 0)
+			return false;
+	}
+	if (i*i == num)
+		return false;
+	return true;
+}
+
+int bj_3671() {
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; ++i) {
+		int ans=0;
+		string s;
+		cin >> s;
+		vector<int> v;
+		set<int> check;
+		for (int j = 0; j < s.length(); ++j)
+			v.push_back(s[j] - '0');
+
+		sort(v.begin(), v.end());
+		do {
+			for (int p = 1; p <= v.size(); p++) {
+				string tmp;
+				bool flag = false;
+				for (int k = 0; k < p; ++k) {
+					if (!flag && v[k] != 0)
+						flag = true;
+					if (flag)
+						tmp += to_string(v[k]);
+				}
+				int n = atoi(tmp.c_str());
+				if (check.count(n) == 0 && n > 1 && check_spy(n)) {
+					check.insert(n);
+					ans++;
+				}
+			}
+		} while (next_permutation(v.begin(), v.end()));
+		cout << ans << '\n';
+	}
+	return 0;
+}
