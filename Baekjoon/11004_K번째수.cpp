e659ba2413f913b85e001b315af9dcd926c177bc
wh2per <93negaro@gmail.com>
2019-03-30 16:19:23 +0900

    11004_K번째수

---
+++ Baekjoon/11004_K번째수.cpp
@@ -0,0 +1,26 @@
+﻿#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int bj_11004() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> v;
+	for (int i = 0; i < n; ++i) {
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+
+	sort(v.begin(), v.end());
+
+	cout << v[m - 1];
+
+	return 0;
+}
