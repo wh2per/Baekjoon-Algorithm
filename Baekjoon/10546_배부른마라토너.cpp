b32a27a5e34f9bec3f4a4242f1139c43e628d084
wh2per <93negaro@gmail.com>
2019-03-31 00:13:17 +0900

    10546_배부른마라토너

---
+++ Baekjoon/10546_배부른마라토너.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
+
+int bj_10546() {
+	int N;
+	cin >> N;
+	map<string, int> m;
+	for (int i = 0; i < N; ++i) {
+		string name;
+		cin >> name;
+		if (m.count(name) == 0)
+			m.insert({ name,1 });
+		else
+			m[name]++;
+	}
+
+	for (int i = 0; i < N - 1; ++i) {
+		string name;
+		cin >> name;
+		if (m[name] == 1)
+			m.erase(name);
+		else
+			m[name]--;
+	}
+
+	cout << (m.begin())->first;
+	return 0;
+}
