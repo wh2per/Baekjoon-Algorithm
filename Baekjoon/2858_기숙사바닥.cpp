e2a983b3288ad5d0b74d5b76fee7efae3908c99b
wh2per <93negaro@gmail.com>
2019-03-31 00:12:45 +0900

    2858_기숙사바닥

---
+++ Baekjoon/2858_기숙사바닥.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int bj_2858() {
+	int B, R;
+	cin >> R >> B;
+
+	vector<pair<int, int>> v;
+	// 약수 검사
+	for (int i = 1; i*i <= B; ++i) {
+		if (B%i == 0) {		// 약수일때
+			v.push_back({i,B/i});
+		}
+	}
+
+	for (int i = 0; i < v.size(); ++i) {
+		int col = v[i].first;
+		int row = v[i].second;
+		if ((col * 2) + (row + 2) * 2 == R) {
+			cout << row+2  << " " << col+2;
+			return 0;
+		}
+	}
+
+	return 0;
+}
