cab6ece90333cc2e4b6dbfd7db8c7c0fff335c6e
wh2per <93negaro@gmail.com>
2019-03-30 20:14:52 +0900

    1300_K번째수

---
+++ Baekjoon/1300_K번째수.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int bj_1300() {
+	int ans = 0;
+	int N, K;
+	cin >>N >>K;
+
+	int left = 1;
+	int right = K;
+	int mid;
+	while (left <= right) {
+		mid = (left + right) >> 1;		
+		long long count = 0;
+		for (int i = 1; i <= N; ++i)
+			count += min(N,mid/i);
+		if (count >= K) {
+			right = mid - 1;
+			ans = mid;
+		}
+		else
+			left = mid + 1;
+	}
+	cout << ans;
+	return 0;
+}
