a2d052a06c4fd35008fd91818128f2e798829dbd
wh2per <93negaro@gmail.com>
2019-03-31 00:14:05 +0900

    1966_프린터큐

---
+++ Baekjoon/1966_프린터큐.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int bj_1966() {
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; ++i) {
+		int N, M;
+		cin >> N >> M;
+		queue<pair<int, int>> q;		// <prior, name>
+		vector<int> p;
+		int a = 1;
+		int b = 0;
+
+		for (int j = 0; j < N; ++j) {
+			int prior;
+			cin >> prior;
+			q.push({prior,a});
+			p.push_back(prior);
+			if (j == M)
+				b = a;
+			++a;
+		}
+		sort(p.begin(), p.end(), greater<int>());
+		
+		for (int k = 0; k < p.size(); ++k) {
+			while (q.front().first != p[k]) {		// 찾는 prior가 아니라면 뒤로 보내자
+				q.push(q.front());
+				q.pop();
+			}
+			if (q.front().second == b) {
+				cout << k + 1 << '\n';
+				break;
+			}
+			q.pop();
+		}
+	}
+	return 0;
+}
