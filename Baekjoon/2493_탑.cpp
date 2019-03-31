292eeda8d6eba4798894379b9aed9fc785b37ce5
wh2per <93negaro@gmail.com>
2019-04-01 00:32:54 +0900

    2493_탑

---
+++ Baekjoon/2493_탑.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int bj_2493() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N;
+	cin >> N;
+	stack<pair<int, int>> s;
+	for (int i = 0; i < N; ++i) {
+		int h;
+		cin >> h;
+		while (!s.empty()) {
+			if (s.top().second > h) {
+				cout << s.top().first << " ";
+				break;
+			}
+			s.pop();
+		}
+		if (s.empty())
+			cout << 0 << " ";
+		s.push({ i + 1,h });
+	}
+	return 0;
+}
