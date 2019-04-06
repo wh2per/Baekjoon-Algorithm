10403cf21aa057cfa65ccccb13e50dcc9d26e404
wh2per <93negaro@gmail.com>
2019-04-06 20:40:39 +0900

    1932_정수삼각형

---
+++ Baekjoon/1932_정수삼각형.cpp
@@ -0,0 +1,47 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<vector<int>> triangle;
+
+	for (int i = 1; i <= n; ++i) {
+		vector<int> v;
+		for (int j = 0; j < i; ++j) {
+			int num;
+			cin >> num;
+			v.push_back(num);
+		}
+		triangle.push_back(v);
+	}
+
+	int answer = triangle[0][0];
+
+	if (triangle.size() == 1)
+		return answer;
+
+	for (int i = 1; i < triangle.size(); i++) {			// 2 ÃþºÎÅÍ
+		for (int j = 0; j <= i; j++) {
+			if (j == 0)
+				triangle[i][j] += triangle[i - 1][j];
+			else if (j == triangle[i].size() - 1)
+				triangle[i][j] += triangle[i - 1][j - 1];
+			else
+				triangle[i][j] += max(triangle[i - 1][j], triangle[i - 1][j - 1]);
+		}
+	}
+
+	for (int i = 0; i < triangle.size(); i++) {
+		if (answer < triangle[triangle.size() - 1][triangle.size() - 1 - i])
+			answer = triangle[triangle.size() - 1][triangle.size() - 1 - i];
+	}
+
+	cout << answer;
+	return 0;
+}
+
