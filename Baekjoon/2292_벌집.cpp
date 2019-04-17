e1092a092ec868dc687a03fe2a5b839046b07241
wh2per <93negaro@gmail.com>
2019-04-17 13:50:41 +0900

    2292_벌집

---
+++ Baekjoon/2292_벌집.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_2292() {
+	int n;
+	int count = 1;
+
+	cin >> n;
+	if (n == 1)
+		cout << 1;
+	else {
+		int temp = 1;
+		while (temp < n) {
+			temp += 6 * count;
+			++count;
+		}
+		cout << count;
+	}
+	return 0;
+}
