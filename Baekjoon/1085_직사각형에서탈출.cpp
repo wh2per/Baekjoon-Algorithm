5a868b37af318103b5cf1bbac07760f22678f25e
wh2per <93negaro@gmail.com>
2019-04-17 13:50:06 +0900

    1085_직사각형에서탈출

---
+++ Baekjoon/1085_직사각형에서탈출.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int bj_1085() {
+	int x, y, w, h;
+	cin >> x >> y >> w >> h;
+
+	cout << min(min(w-x,h-y),min(x,y));
+	return 0;
+}
