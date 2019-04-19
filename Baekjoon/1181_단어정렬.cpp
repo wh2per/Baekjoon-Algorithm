1a9e82d4a99126cc13e0a16560c96c2ac2ba6a6b
wh2per <93negaro@gmail.com>
2019-04-19 14:13:26 +0900

    1181_단어정렬

---
+++ Baekjoon/1181_단어정렬.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+struct cmp_word {
+	bool operator()(pair<string, int> p1, pair<string, int> p2) {
+		if (p1.second < p2.second) {
+			return true;
+		}
+		else if (p1.second > p2.second) {
+			return false;
+		}
+		else {
+			if (p1.first < p2.first)
+				return true;
+			else
+				return false;
+		}
+	}
+};
+
+int bj_1181() {
+	int n;
+	cin >> n;
+	vector<pair<string, int>> m;
+
+	for (int i = 0; i < n; ++i) {
+		string s;
+		cin >> s;
+		m.push_back({s,s.length()});
+	}
+
+	sort(m.begin(), m.end(), cmp_word());
+	string temp = "";
+	for (auto a : m) {
+		if(a.first!=temp)
+			cout << a.first << '\n';
+		temp = a.first;
+	}
+	return 0;
+}
