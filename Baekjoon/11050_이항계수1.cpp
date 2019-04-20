e1ea05b864a0ce4e72ba1f7cc61a89688fde3b6f
wh2per <93negaro@gmail.com>
2019-04-20 19:51:25 +0900

    11050_이항계수1

---
+++ Baekjoon/11050_이항계수1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_11050(){
+	int n, k;
+	cin >> n >> k;
+
+	int first = 1;
+	for (int i = 0; i < n; ++i)
+		first *= (i + 1);
+	int second = 1;
+	for (int i = 0; i < k; ++i)
+		second *= (i + 1);
+	int temp = n - k;
+	int third = 1;
+	for (int i = 0; i < temp; ++i)
+		third *= (i + 1);
+	cout << first / second / third;
+	return 0;
+}
--- Baekjoon/16235_나무제테크.cpp
+++ Baekjoon/16235_나무제테크.cpp
@@ -7,126 +7,95 @@ using namespace std;
 const int dx[] = {1,1,0,-1,-1,-1,0,1};		// 오른쪽 부터 시계방향
 const int dy[] = {0,1,1,1,0,-1,-1,-1};
 
-int bj_16235() {
-	int tree_ans = 0;
-	map<pair<int, int>, vector<int>> tree;
-	map<pair<int, int>, vector<int>> dead;
-	int N, M, K, x, y, z;
+struct info_tree {
+	int y;
+	int x;
+	int count;
+};
 
+struct cmp_tree {
+	bool operator()(info_tree& if1, info_tree& if2) {
+		if (if1.count < if2.count)
+			return true;
+		else
+			return false;
+	}
+};
+
+int main() {
+	int N, M, K;
 	cin >> N >> M >> K;
-	vector<vector<int>> map(N, vector<int>(N, 5));
+	vector<vector<int>> map_tree(N, vector<int>(N, 5));
 	vector<vector<int>> A(N, vector<int>(N, 0));
-	for (int i = 0; i < N; ++i) {			// 맵 입력
-		for (int j = 0; j < N; ++j)
+	vector<info_tree> tree;
+
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) 
 			cin >> A[i][j];
 	}
 
-	for (int i = 0; i < M; i++) {		// 나무 입력
-		cin >> x >> y >> z;
-		if (tree.count({ x - 1,y - 1 }) > 0)				// (1,1)부터 시작
-			tree[{x - 1, y - 1}].push_back(z);
-		else
-			tree.insert({ { x - 1,y - 1 }, {z} });
+	for (int i = 0; i < M; ++i) {
+		int y, x, count;
+		cin >> y >> x >> count;
+		tree.push_back({y-1,x-1,count});
 	}
-
 
 	for (int z = 0; z < K; ++z) {
 		// 봄
-		vector<pair<int, int>> temp;
-		for (auto a : tree) {
-			int ty = a.first.first;
-			int tx = a.first.second;
-			vector<int> t = a.second;
-			bool flag = true;
-			int idx = 0;
+		sort(tree.begin(), tree.end(), cmp_tree());
+		vector<info_tree> tree_next;
+		vector<info_tree> tree_dead;
+		for (int i = 0; i < tree.size(); ++i) {
+			int y = tree[i].y;
+			int x = tree[i].x;
+			int count = tree[i].count;
 
-			sort(t.begin(), t.end());
-			while (flag) {
-				if (map[ty][tx] >= t[idx]) {		// 가장 어린 나무의 나이보다 양분이 같거나 많음
-					map[ty][tx] -= t[idx];		// 양분먹고
-					t[idx]++;					// 나이도 먹고
-					idx++;
-					if (idx >= t.size()) {
-						tree[{ty, tx}] = t;
-						flag = false;
-					}
-				}
-				else {
-					flag = false;
-					if (idx == 0) {			// 가장 어린 나무였으면 모든 나무 죽음
-						dead.insert(a);		// 죽은 나무에 추가
-						temp.push_back({ ty,tx });
-					}
-					else {				// 일부 나무만 죽어야 할때
-						int size = t.size();
-						vector<int> d;
-						for (int k = 0; k < size - idx; ++k) {
-							d.push_back(t.back());
-							t.pop_back();
-						}
-						dead.insert({ { ty,tx }, d });
-						tree[{ty, tx}] = t;		// 해당 지역 나무 갱신
-					}
-				}
+			if (count > map_tree[y][x])
+				tree_dead.push_back(tree[i]);		// 죽은 나무 추가
+			else {
+				map_tree[y][x] -= count;
+				++count;
+				tree_next.push_back({ y,x,count });
 			}
 		}
-		for (int e = 0; e < temp.size(); ++e) 
-			tree.erase({temp[e]});
-		
 
-		vector<pair<int,int>> temp2;
 		// 여름
-		for (auto a : dead) {			// 죽은 나무들 꺼내서
-			int ty = a.first.first;
-			int tx = a.first.second;
-			temp2.push_back({ ty,tx });
-			vector<int> d = a.second;
-			for (int k = 0; k < d.size(); ++k)
-				map[ty][tx] += d[k] / 2;
+		for (int i = 0; i < tree_dead.size(); ++i) {
+			int y = tree_dead[i].y;
+			int x = tree_dead[i].x;
+			int count = tree_dead[i].count;
+
+			map_tree[y][x] += count / 2;
 		}
-		for (int e = 0; e < temp2.size(); ++e)
-			dead.erase({ temp2[e] });
 
-		vector<pair<int, int>> temp3;
 		// 가을
-		for (auto a : tree) {
-			int ty = a.first.first;
-			int tx = a.first.second;
-			vector<int> t = a.second;
+		int tsize = tree_next.size();
+		for (int i = 0; i < tsize; ++i) {
+			if (tree_next[i].count % 5 == 0) {		// 5의 배수
+				int y = tree_next[i].y;
+				int x = tree_next[i].x;
+				for (int k = 0; k < 8; ++k) {
+					int ny = y + dy[k];
+					int nx = x + dx[k];
 
-			for (int g = 0; g < t.size(); ++g) {
-				if (t[g] % 5 == 0) {
-					for (int k = 0; k < 8; ++k) {		// 8방향 검사
-						int ky = ty + dy[k];
-						int kx = tx + dx[k];
-
-						if (ky<0 || ky>=N || kx<0 || kx>=N)		// 범위를 벗어나면 pass
-							continue;
-
-						temp3.push_back({ky,kx});
-					}
+					if (ny < 0 || ny >= N || nx < 0 || nx >= N)
+						continue;
+					
+					tree_next.push_back({ny,nx,1});
 				}
 			}
 		}
-		for (int e = 0; e < temp3.size(); ++e) {
-			if (tree.count(temp3[e]) == 0)
-				tree.insert({ temp3[e],{1} });
-			else
-				tree[temp3[e]].push_back(1);
-		}
+
+		tree = tree_next;
 
 		// 겨울
-		for (int q = 0; q < N; ++q) {
-			for (int w = 0; w < N; ++w)
-				map[q][w] += A[q][w];
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N; ++j)
+				map_tree[i][j] += A[i][j];
 		}
 	}
 
-	for (auto a : tree) {
-		tree_ans += a.second.size();
-	}
-
-	cout << tree_ans;
+	cout << tree.size();
 
 	return 0;
 }
