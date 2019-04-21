f156fdf9c3026a6550bd8770650454065f13c79f
wh2per <93negaro@gmail.com>
2019-04-22 01:09:13 +0900

    2164_카드2

---
+++ Baekjoon/2164_카드2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <list>
+using namespace std;
+
+int bj_2164() {
+	list<int> l;
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; ++i)
+		l.push_back(i + 1);
+
+	while (l.size() > 1) {
+		l.pop_front();
+		int t = *l.begin();
+		l.pop_front();
+		l.push_back(t);
+	}
+
+	cout << *l.begin();
+	return 0;
+}
