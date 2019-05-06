3787832b00bd47dc6ebee8531f76f9dc73505b6d
wh2per <93negaro@gmail.com>
2019-05-07 00:38:52 +0900

    9020_골드바흐의추측

---
+++ Baekjoon/9020_골드바흐의추측.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int bj_9020() {
+	vector<bool> check(20001, false);
+	int temp = 20000;
+	int pn = 0;
+	for (int i = 2; i*i <= temp; ++i) {
+		if (!check[i]) {
+			for (int j = i * i; j <= temp; j += i)
+				check[j] = true;
+		}
+	}
+
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; ++i) {
+		int num = -1;
+		cin >> num;
+		for (int j = num/2; j>0; --j) {
+			if (!check[j] && !check[num - j]) {
+				cout << j << " " << num-j << '\n';
+				break;
+			}
+		}
+	}
+	
+	return 0;
+}
