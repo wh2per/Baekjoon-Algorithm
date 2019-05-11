1115981ad46ac05830e8045387263430be3e5883
wh2per <93negaro@gmail.com>
2019-05-12 00:15:39 +0900

    10773_제로

---
+++ Baekjoon/10773_제로.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int bj_10773() {
+	vector<int> v;
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; ++i) {
+		int num;
+		cin >> num;
+		if (num != 0)
+			v.push_back(num);
+		else
+			v.pop_back();
+	}
+
+	int ans = 0;
+	for (auto a : v)
+		ans += a;
+	cout << ans;
+	return 0;
+}
