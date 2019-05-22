10c91c31423fc3bcf33daf548269c3b547c97607
wh2per <93negaro@gmail.com>
2019-05-23 00:14:37 +0900

    4949_균형잡힌세상

---
+++ Baekjoon/4949_균형잡힌세상.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int bj_4949() {
+	while (1) {
+		string str;
+		getline(cin, str);		// 띄어쓰기 포함 입력
+		if (str == ".") {
+			break;
+		}
+
+		bool flag = true;
+		stack<char> s;
+		for (int i = 0; i < str.length(); ++i) {
+			if (str[i] == ')') {
+				if (s.empty()) {
+					flag = false;
+					break;
+				}
+				else {
+					if (s.top() == '(') {
+						s.pop();
+					}
+					else {
+						flag = false;
+						break;
+					}
+				}
+			}
+			else if (str[i] == ']') {
+				if (s.empty()) {
+					flag = false;
+					break;
+				}
+				else {
+					if (s.top() == '[') {
+						s.pop();
+					}
+					else {
+						flag = false;
+						break;
+					}
+				}
+			}
+			else if (str[i] == '(') {
+				s.push(str[i]);
+			}
+			else if (str[i] == '[') {
+				s.push(str[i]);
+			}
+			else
+				continue;
+		}
+		if (!flag || s.size() > 0)
+			cout << "no" << " " << '\n';
+		else
+			cout << "yes" << " " << '\n';
+	}
+	return 0;
+}
