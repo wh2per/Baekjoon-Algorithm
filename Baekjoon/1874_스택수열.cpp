6bc4deabfd80881eb6d9c57add0cbbe819a01cea
wh2per <93negaro@gmail.com>
2019-05-31 18:06:21 +0900

    1874_스택수열

---
+++ Baekjoon/1874_스택수열.cpp
@@ -0,0 +1,32 @@
+﻿#include <stack>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int bj_1874() {
+	stack<int> s;
+	vector<char> ans;
+	int n; 
+	cin >> n;
+	int start = 1;
+	int count = n;
+	for (int i = 0; i < n; ++i) {
+		int target;
+		cin >> target;
+
+		while (count>=0 && (s.empty() || s.top() != target)) {
+			s.push(start++);
+			ans.push_back('+');
+			--count;
+		}
+		if (count < 0) {
+			cout << "NO";
+			return 0;
+		}
+		s.pop();
+		ans.push_back('-');
+	}
+	for (auto a : ans)
+		cout << a << '\n';
+	return 0;
+}
