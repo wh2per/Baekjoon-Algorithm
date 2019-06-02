93783625da39eac8b14e3e97b8331ec2975f5d1a
wh2per <93negaro@gmail.com>
2019-06-02 17:06:31 +0900

    2609_최대공약수와최소공배수

---
+++ Baekjoon/2609_최대공약수와최소공배수.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int bj_2609() {
+	int a, b;
+	cin >> a >> b;
+
+	int f = max(a, b);
+	int s = min(a, b);
+
+	while (s != 0) {
+		int r = f % s;
+		f = s;
+		s = r;
+	}
+
+	cout << f<<'\n';
+	cout << a * b / f;
+	return 0;
+}
