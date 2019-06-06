1553e8572ce4d07b04992df13fae2a63d7f491b1
wh2per <93negaro@gmail.com>
2019-06-06 14:53:19 +0900

    9461_파도반수열

---
+++ Baekjoon/9461_파도반수열.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int bj_9461() {
+	int t; cin >> t;
+	vector<long long> d(101);
+	d[1] = 1;
+	d[2] = 1;
+	d[3] = 1;
+	d[4] = 2;
+	d[5] = 2;
+	for (int i = 6; i < 101; ++i)
+		d[i] = d[i - 1] + d[i - 5];
+	for (int i = 0; i < t; ++i) {
+		int num; cin >> num;
+		cout << d[num] << "\n";
+	}
+	return 0;
+}
