fead80a5db4d53179c7ffe307ea58b97fbad1348
wh2per <93negaro@gmail.com>
2019-06-07 16:14:41 +0900

    1351_무한수열

---
+++ Baekjoon/1351_무한수열.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+long long inf_d[41][41];
+long long inf_p, inf_q;
+
+long long inf_dp(long long n, long long p, long long q) {
+	if (n == 0)
+		return 1;
+	if (inf_d[p][q] != 0)
+		return inf_d[p][q];
+	return inf_d[p][q] = inf_dp(n / inf_p, p + 1, q) + inf_dp(n / inf_q, p, q + 1);
+}
+
+int bj_1351() {
+	long long n;
+	cin >> n >> inf_p >> inf_q;
+	cout << inf_dp(n, 0, 0);
+	return 0;
+}
