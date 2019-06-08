3528f99ddaa70be0b9dab2c44361083513a2710b
wh2per <93negaro@gmail.com>
2019-06-09 00:31:19 +0900

    2293_동전1

---
+++ Baekjoon/2293_동전1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int bj_2293() {
+	int n, k;
+	cin >> n >> k;
+	vector<int> coin(n);
+	vector<int> d(k + 1, 0);
+
+	for (int i = 0; i < n; ++i)
+		cin >> coin[i];
+	d[0] = 1;
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 1; j <= k; ++j) {
+			if (j - coin[i] >= 0)
+				d[j] += d[j - coin[i]];
+		}
+	}
+	cout << d[k];
+	return 0;
+}
