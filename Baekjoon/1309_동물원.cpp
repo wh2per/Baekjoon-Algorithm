71a3002a0c558d8144acc8079bbe0578136bc7ec
wh2per <93negaro@gmail.com>
2019-06-10 00:16:40 +0900

    1309_동물원

---
+++ Baekjoon/1309_동물원.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int bj_1309() {
+	int n;
+	cin >> n;
+	vector<vector<int>> d(n + 1, vector<int>(3, 0));
+	d[1][0] = 1;
+	d[1][1] = 1;
+	d[1][2] = 1;
+
+	for (int i = 2; i <= n; ++i) {
+		d[i][0] = (d[i - 1][0] + d[i - 1][1] + d[i - 1][2]) % 9901;
+		d[i][1] = (d[i-1][0] + d[i-1][2]) % 9901;
+		d[i][2] = (d[i-1][0] + d[i-1][1]) % 9901;
+	}
+	cout << (d[n][0] + d[n][1] + d[n][2]) % 9901;
+	return 0;
+}
