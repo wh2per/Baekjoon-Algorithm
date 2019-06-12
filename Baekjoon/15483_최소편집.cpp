1598f905d02f3815b8fec514d6391b29063f9b9c
wh2per <93negaro@gmail.com>
2019-06-12 21:50:35 +0900

    15483_최소편집

---
+++ Baekjoon/15483_최소편집.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int bj_15483() {
+	string s1, s2;
+	cin >> s1 >> s2;
+	vector<vector<int>> d(1001, vector<int>(1001, 0));
+	int len1 = s1.length();
+	int len2 = s2.length();
+	
+	for (int i = 0; i <= len1; ++i)
+		d[i][0] = i;
+
+	for (int i = 0; i <= len2; ++i)
+		d[0][i] = i;
+	
+	for (int i = 1; i <= len1; i++) { 
+		for (int j = 1; j <= len2; j++) { 
+			if (s1[i - 1] == s2[j - 1]) 
+				d[i][j] = d[i - 1][j - 1]; 
+			else 
+				d[i][j] = min(d[i - 1][j - 1] + 1, min(d[i][j - 1] + 1, d[i - 1][j] + 1));
+		} 
+	}
+
+	cout << d[len1][len2];
+	return 0;
+}
