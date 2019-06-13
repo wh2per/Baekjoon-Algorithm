202b0fd71bd0fc2cd490eb30f970f1d35bf60e52
wh2per <93negaro@gmail.com>
2019-06-14 02:18:43 +0900

    14490_백대열

---
+++ Baekjoon/14490_백대열.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int bj_14490() {
+	string s;
+	cin >> s;
+	int index = s.find(":");
+	int a = stoi(s.substr(0, index));
+	int b = stoi(s.substr(index + 1, s.length()));
+	
+	int g = a;
+	int t = b;
+	int c;
+	while (t != 0)
+	{
+		c = g % t;
+		g = t;
+		t = c;
+	}
+
+	a /= g;
+	b /= g;
+	cout << a << ":" << b;
+	return 0;
+}
