59caa90479860096e7f0dd95613fb5ae9f2c77d0
wh2per <93negaro@gmail.com>
2019-06-15 22:45:23 +0900

    1094_막대기

---
+++ Baekjoon/1094_막대기.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_1094() {
+	int target;
+	cin >> target;
+
+	int ans = 0;
+	while (target !=0) {
+		if (target % 2 == 1)
+			++ans;
+		target /= 2;
+	}
+	cout << ans;
+
+	return 0;
+}
