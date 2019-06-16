c9526a461fc5c88d478b31ab16bc564c92825b44
wh2per <93negaro@gmail.com>
2019-06-16 22:55:42 +0900

    1850_최대공약수

---
+++ Baekjoon/1850_최대공약수.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_1850() {
+	// 1의 개수의 최대공약수가 결과 수의 1의 개수
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	long long a, b;
+	cin >> a >> b;
+
+	while (b != 0) {
+		long long r = a % b;
+		a = b;
+		b = r;
+	}
+
+	for (int i = 0; i < a; ++i)
+		cout << 1;
+	
+	return 0;
+}
