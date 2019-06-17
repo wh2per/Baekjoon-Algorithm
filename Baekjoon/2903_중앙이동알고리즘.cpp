7d7ea560e80ceed250f3a6ea4ee85e4d0e2ab734
wh2per <93negaro@gmail.com>
2019-06-17 19:11:02 +0900

    2903_중앙이동알고리즘

---
+++ Baekjoon/2903_중앙이동알고리즘.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_2903() {
+	int n;
+	cin >> n;
+	int ans = 2;
+	for (int i = 0; i < n; ++i)
+		ans = 2 * ans - 1;
+	cout << ans * ans;
+	return 0;
+}
