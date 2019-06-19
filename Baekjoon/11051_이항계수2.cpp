6d8e7928f355501ee27323d298e17a72570729d6
wh2per <93negaro@gmail.com>
2019-06-19 15:59:38 +0900

    11051_이항계수2

---
+++ Baekjoon/11051_이항계수2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int ncr_d[1000][1000];
+long long ncr(int n, int r) {
+	if (ncr_d[n][r] > 0)
+		return ncr_d[n][r];
+
+	if (n == r || r == 0)
+		return ncr_d[n][r] = 1;
+
+	return ncr_d[n][r] = (ncr(n - 1, r - 1) + ncr(n - 1, r)) % 10007;
+}
+
+int bj_11501() {
+	int n, r;
+	cin >> n >> r;
+	cout << ncr(n, r);
+	return 0;
+}
