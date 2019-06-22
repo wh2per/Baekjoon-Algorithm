cd1bba43e52e206face1328cb777e1520f34f362
wh2per <93negaro@gmail.com>
2019-06-22 15:34:13 +0900

    4673_셀프넘버

---
+++ Baekjoon/4673_셀프넘버.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int dn(int n) {
+	int value = n;
+	while (n != 0) {
+		value += n % 10;
+		n /= 10;
+	}
+	
+	return value;
+}
+
+int bj_4673() {
+	vector<bool> self(10037, true);
+	for (int i = 1; i <= 10000; ++i) {
+		int v = dn(i);
+		if (self[v])
+			self[v] = false;
+	}
+
+	for (int i = 1; i <= 10000; ++i) {
+		if (self[i])
+			cout << i << '\n';
+	}
+	return 0;
+}
