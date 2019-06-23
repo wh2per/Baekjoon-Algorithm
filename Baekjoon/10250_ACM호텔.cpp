deaea2b1516575e7591a5c162760b6bd5afea8cf
wh2per <93negaro@gmail.com>
2019-06-23 13:02:10 +0900

    10250_ACM호텔

---
+++ Baekjoon/10250_ACM호텔.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int bj_10250() {
+	int t;
+	cin >> t;
+	while (t--) {
+		int h, w, num;
+		string ans = "";
+		cin >> h >> w >> num;
+		
+		int a = (num / h)+1;
+		int b = num % h;
+		if (b == 0) {
+			b = h;
+			--a;
+		}
+
+		ans += to_string(b);
+		if (a < 10)
+			ans += "0";
+		ans += to_string(a);
+		cout << ans << '\n';
+	}
+	return 0;
+}
