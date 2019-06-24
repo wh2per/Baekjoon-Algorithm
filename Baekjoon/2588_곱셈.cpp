2057cd75564d9d44565af961959eedb32194702e
wh2per <93negaro@gmail.com>
2019-06-24 13:12:32 +0900

    2588_곱셈

---
+++ Baekjoon/2588_곱셈.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_2588() {
+	int a, b, temp;
+	cin >> a >> b;
+	int ans = a * b;
+	
+	for (int i = 0; i < 3; ++i) {
+		int temp = b % 10;
+		cout << a * temp << '\n';
+		b /= 10;
+	}
+	cout << ans;
+	
+	return 0;
+}
