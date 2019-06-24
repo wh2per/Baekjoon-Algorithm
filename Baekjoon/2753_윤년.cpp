9f7084610d5d1c738754ef4f97b7e6bab1558d42
wh2per <93negaro@gmail.com>
2019-06-24 13:19:39 +0900

    2753_윤년

---
+++ Baekjoon/2753_윤년.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_2753() {
+	int a;
+	cin >> a;
+	if (a % 4 == 0) {
+		if (a % 100 != 0) {
+			cout << 1;
+		}
+		else if(a % 400==0){
+			cout << 1;
+		}
+		else
+			cout << 0;
+	}else
+		cout << 0;
+	return 0;
+}
