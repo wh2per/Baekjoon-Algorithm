eed7eb8cce935104dab8e5aa3b3460dbd4d95620
wh2per <93negaro@gmail.com>
2019-06-24 13:14:54 +0900

    1330_두수비교하기

---
+++ Baekjoon/1330_두수비교하기.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_1330() {
+	int a, b;
+	cin >> a >> b;
+	if (a > b)
+		cout << ">";
+	else if (a < b)
+		cout << "<";
+	else
+		cout << "==";
+	return 0;
+}
