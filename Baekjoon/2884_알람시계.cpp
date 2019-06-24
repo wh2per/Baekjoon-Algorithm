ba37256c4e5e605a3b5030dfda56a4377f44f57b
wh2per <93negaro@gmail.com>
2019-06-24 13:36:36 +0900

    2884_알람시계

---
+++ Baekjoon/2884_알람시계.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_2884() {
+	int a, b;
+	cin >> a >> b;
+	int time = a * 60 + b;
+	if (time < 45)
+		time += 1440;
+	time -= 45;
+	
+	if (time / 60 != 0)
+		cout << time / 60 << " "; 
+	if(time%60!=0)
+		cout <<time % 60;
+	return 0;
+}
