a51fc1598eede4bf2ac6e75a92ccc537b02005e6
wh2per <93negaro@gmail.com>
2019-06-24 13:06:33 +0900

    10171_고양이

---
+++ Baekjoon/10171_고양이.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_10171() {
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+
+	return 0;
+}
