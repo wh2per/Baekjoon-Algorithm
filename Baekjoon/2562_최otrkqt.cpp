6aeaf9a564f941cc41d731f90e3ffae17b47860e
wh2per <93negaro@gmail.com>
2019-06-25 13:55:32 +0900

    2562_최댓값

---
+++ Baekjoon/2562_최otrkqt.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_2562() {
+	int a,prev = 0;
+	int index, ans;
+	for (int i = 1; i < 10; ++i) {
+		cin >> a;
+		if (a > prev) {
+			index = i;
+			prev = a;
+		}
+	}
+	cout << prev << endl;
+	cout << index;
+	return 0;
+}
