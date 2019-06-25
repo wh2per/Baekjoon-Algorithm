6c32c82b802c778992591e0388e80209a61f18c3
wh2per <93negaro@gmail.com>
2019-06-25 13:59:28 +0900

    2577_숫자의개수

---
+++ Baekjoon/2577_숫자의개수.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int bj_2577() {
+	vector < int> v(10, 0);
+
+	int a, b, c, ans;
+	cin >> a >> b >> c;
+	ans = a * b * c;
+
+	while (ans != 0) {
+		int idx = ans % 10;
+		++v[idx];
+		ans /= 10;
+	}
+
+	for (auto a : v)
+		cout << a << endl;
+	return 0;
+}
