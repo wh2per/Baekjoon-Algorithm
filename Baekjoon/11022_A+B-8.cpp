751462f4bea173528816d06723be7dd9a2600099
wh2per <93negaro@gmail.com>
2019-06-25 13:42:33 +0900

    11022_A+B-8

---
+++ Baekjoon/11022_A+B-8.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_11022() {
+	int t;
+	cin >> t;
+
+	for (int i = 1; i <= t; ++i) {
+		int a, b;
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a<<" + "<<b<<" = "<<a + b << endl;
+	}
+	return 0;
+}
