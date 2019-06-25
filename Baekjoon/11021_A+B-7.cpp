6e80c8e8167d75e4581d3c3c198e5c2ad9352c58
wh2per <93negaro@gmail.com>
2019-06-25 13:41:02 +0900

    11021_A+B-7

---
+++ Baekjoon/11021_A+B-7.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_11021() {
+	int t;
+	cin >> t;
+
+	for (int i = 1; i <= t; ++i) {
+		int a, b;
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a + b<<endl;
+	}
+	return 0;
+}
