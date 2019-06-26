d649f08ab01b77a70dfb5935ea0a751799346f51
wh2per <93negaro@gmail.com>
2019-06-26 15:21:41 +0900

    8958_OX퀴즈

---
+++ Baekjoon/8958_OX퀴즈.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int bj_8958() {
+	int t; cin >> t;
+	while (t--) {
+		string s;
+		cin >> s;
+		int ans = 0;
+		int acc = 0;
+		for (int i = 0; i < s.length(); ++i) {
+			if (s[i] == 'O') {
+				++acc;
+				ans += acc;
+			}
+			else 
+				acc = 0;
+		}
+		cout << ans << '\n';
+	}
+	return 0;
+}
