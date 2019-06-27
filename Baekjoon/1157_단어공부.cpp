cdb12a6e1560679719ab3bf14df0aef7c0d82c19
wh2per <93negaro@gmail.com>
2019-06-27 17:11:42 +0900

    1157_단어공부

---
+++ Baekjoon/1157_단어공부.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int bj_1157() {
+	vector<int> v(26, 0);
+	string str;
+	cin >> str;
+
+	for (auto a : str)
+		++v[tolower(a) - 'a'];
+
+	int max = -1;
+	bool flag = false;
+	char ans = 'a';
+	for (int i = 0; i < 26; ++i) {
+		if (max < v[i]) {
+			max = v[i];
+			flag = false;
+			ans = toupper(i + 'a');
+		}
+		else if (max == v[i])
+			flag = true;
+	}
+
+	if (flag)
+		cout << "?";
+	else
+		cout << ans;
+	return 0;
+}
