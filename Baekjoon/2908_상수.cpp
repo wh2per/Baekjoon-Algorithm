b52d43137466ff1fe62d1832a24d929ead9c1bc1
wh2per <93negaro@gmail.com>
2019-06-27 17:16:35 +0900

    2908_상수

---
+++ Baekjoon/2908_상수.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int bj_2908() {
+	string n1, n2;
+	cin >> n1 >> n2;
+	reverse(n1.begin(), n1.end());
+	reverse(n2.begin(), n2.end());
+	int a1 = stoi(n1);
+	int a2 = stoi(n2);
+	if (a1 > a2)
+		cout << a1;
+	else
+		cout << a2;
+	return 0;
+}
