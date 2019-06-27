21e59afbd8ddaa84eaaf027d2340e75e34ed9658
wh2per <93negaro@gmail.com>
2019-06-27 15:29:52 +0900

    2920_음계

---
+++ Baekjoon/2920_음계.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int bj_2920() {
+	vector<int> v(8,0);
+	for (int i = 0; i < 8; ++i)
+		cin >> v[i];
+
+	int j = 0;
+	int prev = v[0];
+	int count = 0;
+	for (j = 1; j < 8; ++j) {
+		if (prev < v[j])
+			++count;
+		else
+			--count;
+		prev = v[j];
+	}
+
+	if (count == 7)
+		cout << "ascending";
+	else if (count == -7)
+		cout << "descending";
+	else
+		cout << "mixed";
+	return 0;
+}
