1e6669889ab03ec7f809488873cc1aaa7b233941
wh2per <93negaro@gmail.com>
2019-06-28 00:38:43 +0900

    5622_다이얼

---
+++ Baekjoon/5622_다이얼.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int bj_5622() {
+	string str;
+	cin >> str;
+	int ans = 0;
+
+	for (auto a : str) {
+		int temp = a - 'A';
+		if (temp > 17 && temp!=25)
+			--temp;
+		if (temp == 25)
+			temp -= 2;
+		temp /= 3;
+		temp += 3;
+		ans += temp;
+	}
+	cout << ans;
+	return 0;
+}
