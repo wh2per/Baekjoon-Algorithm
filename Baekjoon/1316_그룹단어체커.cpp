ff26fcfe915e76e27ad5ec35b3aab652f17de7b7
wh2per <93negaro@gmail.com>
2019-06-28 00:56:17 +0900

    1316_그룹단어체커

---
+++ Baekjoon/1316_그룹단어체커.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int bj_1316() {
+	int t;
+	cin >> t;
+	int ans = 0;
+
+	while (t--) {
+		string str;
+		cin >> str;
+		vector<bool> v(26, false);
+
+		int prev = str[0] - 'a';
+		int i = 0;
+		for (i = 0; i < str.length(); ++i) {
+			int temp = str[i] - 'a';
+			if (prev != temp && v[temp])
+				break;
+			v[temp] = true;
+			prev = temp;
+		}
+
+		if (i == str.length())
+			++ans;
+	}
+
+	cout << ans;
+	return 0;
+}
