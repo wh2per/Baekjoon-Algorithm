ec956e7c88e9a61136eaa48064307bf23656c293
wh2per <93negaro@gmail.com>
2019-06-27 15:55:13 +0900

    10870_피보나치수5

---
+++ Baekjoon/10870_피보나치수5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int fibo5(int n) {
+	if (n == 0)
+		return 0;
+	if (n == 1)
+		return 1;
+	return fibo5(n - 2) + fibo5(n - 1);
+}
+
+int bj_10870() {
+	int n;
+	cin >> n;
+
+	cout << fibo5(n);
+	return 0;
+}
