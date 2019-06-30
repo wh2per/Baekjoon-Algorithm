cfaff84d9989bc9f5ddc5b75f425b5cdd5437de1
wh2per <93negaro@gmail.com>
2019-07-01 00:05:58 +0900

    3052_나머지

---
+++ Baekjoon/3052_나머지.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <set>
+using namespace std;
+
+int bj_3052() {
+	int t = 10;
+	set<int> s;
+	while (t--) {
+		int a;
+		cin >> a;
+		s.insert(a % 42);
+	}
+	cout << s.size();
+	return 0;
+}
