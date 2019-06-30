413aaa981fb1d0bcd99b15993227db4488e98605
wh2per <93negaro@gmail.com>
2019-06-30 14:06:47 +0900

    1427_소트인사이드

---
+++ Baekjoon/1427_소트인사이드.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int bj_1427() {
+	string str;
+	cin >> str;
+	vector<char> v;
+
+	for (auto a : str)
+		v.push_back(a);
+	sort(v.begin(), v.end());
+
+	for (int i = v.size()-1; i >= 0; --i)
+		cout << v[i];
+	return 0;
+}
