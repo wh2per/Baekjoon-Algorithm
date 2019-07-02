ee2f41d83637ec058b523f7510a30f7ba3a5e229
wh2per <93negaro@gmail.com>
2019-07-03 02:06:50 +0900

    3053_택시기하학

---
+++ Baekjoon/3053_택시기하학.cpp
@@ -0,0 +1,15 @@
+﻿#define _USE_MATH_DEFINES
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int bj_3053(){
+	int x;
+	cin >> x;
+
+	cout << fixed;
+	cout.precision(6);
+
+	cout << x * x*M_PI << endl;
+	cout << x * x * 2.0 << endl;
+}
