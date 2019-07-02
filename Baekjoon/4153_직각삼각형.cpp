06ae5af9377a70cd63257e3b2f98e9fa9a97c76f
wh2per <93negaro@gmail.com>
2019-07-03 01:48:09 +0900

    4153_직각삼각형

---
+++ Baekjoon/4153_직각삼각형.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int bj_4153() {
+	vector<int> v(3);
+	while (1) {
+		cin >> v[0] >> v[1] >> v[2];
+		if (v[0] == 0 && v[1] == 0 && v[2] == 0)
+			break;
+
+		sort(v.begin(), v.end());
+		if (v[2]*v[2] == v[1] * v[1] + v[0] * v[0])
+			cout << "right\n";
+		else
+			cout << "wrong\n";
+	}
+	return 0;
+}
