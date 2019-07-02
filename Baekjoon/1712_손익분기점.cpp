c32afdb06807254845042edb3dc0d621d42aed03
wh2per <93negaro@gmail.com>
2019-07-02 22:46:31 +0900

    1712_손익분기점

---
+++ Baekjoon/1712_손익분기점.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_1712() {
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	if (b >= c)
+		cout << -1;
+	else {
+		int ans = a / (c - b);
+		cout << ++ans;
+	}
+	
+	return 0;
+}
