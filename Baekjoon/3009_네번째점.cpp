d8b158fd5417a1e83bf8711bc3e43375320d9f6f
wh2per <93negaro@gmail.com>
2019-07-03 01:42:36 +0900

    3009_네번째점

---
+++ Baekjoon/3009_네번째점.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_3009() {
+	int px1, py1, px2, py2, px3, py3, px4, py4;
+	cin >> px1 >> py1 >> px2 >> py2 >> px3 >> py3;
+
+	if (px1 == px2)
+		px4 = px3;
+	else {
+		if (px1 == px3)
+			px4 = px2;
+		else
+			px4 = px1;
+	}
+	
+	if (py1 == py2)
+		py4 = py3;
+	else {
+		if (py1 == py3)
+			py4 = py2;
+		else
+			py4 = py1;
+	}
+
+	cout << px4 << " " << py4;
+	return 0;
+}
