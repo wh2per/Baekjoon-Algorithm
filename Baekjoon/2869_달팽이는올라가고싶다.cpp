f27a71ca8012749f2fdec4dd006850d988319737
wh2per <93negaro@gmail.com>
2019-07-03 01:36:36 +0900

    2869_달팽이는올라가고싶다

---
+++ Baekjoon/2869_달팽이는올라가고싶다.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_2869() {
+	int a, b, v;
+	cin >> a >> b >> v; 
+
+	int ans = 1;
+
+	if (a >= v)
+		cout << ans;
+	else {
+		int temp = v - a;
+		ans += temp / (a - b);
+		if (temp % (a - b) != 0)
+			++ans;
+		cout << ans;
+	}
+	return 0;
+}
