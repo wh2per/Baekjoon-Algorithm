f0f7453ffb7264ac8b09a4f4a44d927bcd8089fe
wh2per <93negaro@gmail.com>
2019-07-07 00:50:58 +0900

    2798_블랙잭

---
+++ Baekjoon/2798_블랙잭.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int bj_2798() {
+	int n, jack;
+	cin >> n >> jack;
+	vector<int> v(n,0);
+
+	for (int i = 0; i < n; ++i)
+		cin >> v[i];
+	int ans = -1;
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			for (int k = 0; k < n; ++k) {
+				if (i != j && i != k && j != k) {
+					int sum = v[i] + v[j] + v[k];
+					if (sum<=jack && sum>ans)
+						ans = sum;
+				}
+			}
+		}
+	}
+	cout << ans;
+	return 0;
+}
