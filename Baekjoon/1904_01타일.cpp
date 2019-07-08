93022e76cfa1ad4c2f9d27f6b17a930c809dfd90
wh2per <93negaro@gmail.com>
2019-07-08 20:30:48 +0900

    1904_01타일

---
+++ Baekjoon/1904_01타일.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int bj_1904() {
+	int n;
+	cin >> n;
+
+	vector<long long> d(n + 1, 0);
+
+	d[0] = 1;
+	d[1] = 1;
+
+	for (int i = 2; i < n + 1; ++i)
+		d[i] = (d[i - 1] + d[i - 2]) % 15746;
+
+	cout << d[n];
+	return 0;
+}
