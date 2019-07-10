151eed08b8baa68d354a00d62a1ec52a078d27a6
wh2per <93negaro@gmail.com>
2019-07-10 17:26:28 +0900

    1965_상자넣기

---
+++ Baekjoon/1965_상자넣기.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int bj_1965() {
+	int n;
+	cin >> n;
+
+	vector<int> v(n);
+	vector<int> d(n);
+
+	for (int i = 0; i < n; i++) 
+		cin >> v[i];
+
+	for (int i = 0; i < n; ++i) {
+		d[i] = 1;
+		for (int j = 0; j < i; ++j) {
+			if (v[j] < v[i] && d[j] + 1 > d[i])
+				d[i] = d[j] + 1;
+		}
+	}
+
+	cout << *max_element(d.begin(), d.end()) << '\n';
+	return 0;
+}
