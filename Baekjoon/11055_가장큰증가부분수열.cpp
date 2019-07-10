1d237678738a3795f464be3b2cfb40024ca6d0fb
wh2per <93negaro@gmail.com>
2019-07-11 01:33:28 +0900

    11055_가장큰증가부분수열

---
+++ Baekjoon/11055_가장큰증가부분수열.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int bj_11055() {
+	int n;
+	cin >> n;
+	 
+	vector<int> v(n);
+	vector<int> d(n);
+
+	for (int i = 0; i < n; ++i)
+		cin >> v[i];
+
+	int ans = -1;
+	for (int i = 0; i < n; ++i) {
+		int temp = 0;
+		for (int j = 0; j < i; ++j) {
+			if (v[j] < v[i] && temp < d[j])
+				temp = d[j];
+		}
+		d[i] = temp + v[i];
+		if (ans < d[i])
+			ans = d[i];
+	}
+
+	cout << ans;
+	return 0;
+}
