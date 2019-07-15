677a87efbb81baaf68e3409df7ddc1a56c4ca758
wh2per <93negaro@gmail.com>
2019-07-16 03:00:56 +0900

    11058_크리보드

---
+++ Baekjoon/11058_크리보드.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int bj_11058() {
+	int n;
+	scanf("%d",&n);
+
+	long long d[101] = {0,};
+
+	for (int i = 1; i <= n; ++i) {
+		d[i] = d[i - 1] + 1;
+		for (int j = 1; j <= i - 3; ++j) {
+			long long c = d[i - j - 2] * (j + 1);
+			if (c > d[i])
+				d[i] = c;
+		}
+	}
+	printf("%ld",d[n]);
+	return 0;
+}
