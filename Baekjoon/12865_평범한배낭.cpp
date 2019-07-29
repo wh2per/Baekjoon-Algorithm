6e162e158e609f1561ad9f79da64d6b79d41176c
wh2per <93negaro@gmail.com>
2019-07-29 23:21:48 +0900

    12865_평범한배낭

---
+++ Baekjoon/12865_평범한배낭.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int bj_12865() {
+	int dp[100001] = {0,}; //가방의 용량이 w 까지일 때의 최대 가치합
+
+	int N, K, w, v;
+	cin >> N >> K;
+	while (N--) {
+		cin >> w >> v;
+		for (int i = K; i >= w; i--)
+			dp[i] = max(dp[i], dp[i - w] + v);
+	}
+	cout << dp[K] << '\n';
+	return 0;
+}
