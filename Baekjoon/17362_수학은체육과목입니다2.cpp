684fc7de3c3f5ad7056cf5f0b166e9bc8463b45d
wh2per <93negaro@gmail.com>
2019-07-30 14:51:10 +0900

    17362_수학은체육과목입니다2

---
+++ Baekjoon/17362_수학은체육과목입니다2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int bj_17362() {
+	int n;
+	cin >> n;
+
+	int ret = n % 8;
+	if (ret == 1)
+		cout << 1;
+	else if (ret == 5)
+		cout << 5;
+	else if (ret == 2 || ret == 0)
+		cout << 2;
+	else if (ret == 3 || ret == 7)
+		cout << 3;
+	else
+		cout << 4;
+	return 0;
+}
