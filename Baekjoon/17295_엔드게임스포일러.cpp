5d347cd557127c73983e46933f44d8bc40e607a4
wh2per <93negaro@gmail.com>
2019-08-06 20:14:22 +0900

    17295_엔드게임스포일러

---
+++ Baekjoon/17295_엔드게임스포일러.cpp
@@ -0,0 +1,14 @@
+// This file is a "Hello, world!" in C++ language by GCC for wandbox.
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int bj_17295()
+{
+    string str;
+    cin >> str;
+    cout << "Avengers: Endgame";
+
+    return 0;
+}
