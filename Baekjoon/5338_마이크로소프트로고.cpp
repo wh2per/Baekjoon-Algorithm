e7b597ef703df013ed27e4e0138431a8965b52e0
wh2per <93negaro@gmail.com>
2019-11-03 23:29:43 +0900

    5338_마이크로소프트로고

---
+++ Baekjoon/5338_마이크로소프트로고.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	//freopen("input.txt", "r", stdin);
+
+	cout << "       _.-;;-._\n";
+	cout << "'-..-'|   ||   |\n";
+	cout << "'-..-'|_.-;;-._|\n";
+	cout << "'-..-'|   ||   |\n";
+	cout << "'-..-'|_.-''-._|\n";
+
+	return 0;
+}
