8946054553644bc5503e6a19f494d7abc47f60c4
wh2per <93negaro@gmail.com>
2019-11-04 00:51:05 +0900

    10926_물음표물음표느낌표

---
+++ Baekjoon/10926_물음표물음표느낌표.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	freopen("input.txt", "r", stdin);
+
+	string s;
+	cin >> s;
+
+	cout << s << "??!";
+
+	return 0;
+}
