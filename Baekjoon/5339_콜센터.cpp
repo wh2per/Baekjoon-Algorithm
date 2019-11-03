46bc3b4eac6c9edc11549e771fe0381752cdb805
wh2per <93negaro@gmail.com>
2019-11-04 00:11:09 +0900

    5339_콜센터

---
+++ Baekjoon/5339_콜센터.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	//freopen("input.txt", "r", stdin);
+
+	cout << "     /~\\\n";
+	cout << "    ( oo|\n";
+	cout << "    _\\=/_\n";
+	cout << "   /  _  \\\n";
+	cout << "  //|/.\\|\\\\\n";
+	cout << " ||  \\ /  ||\n";
+	cout << "============\n";
+	cout << "|          |\n";
+	cout << "|          |\n";
+	cout << "|          |\n";
+	return 0;
+}
