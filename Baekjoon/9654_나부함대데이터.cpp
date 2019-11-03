fc61b05b210f48bce913cea89180455b0f229526
wh2per <93negaro@gmail.com>
2019-11-04 00:44:01 +0900

    9654_나부함대데이터

---
+++ Baekjoon/9654_나부함대데이터.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	//freopen("input.txt", "r", stdin);
+
+	cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n";
+	cout << "N2 Bomber      Heavy Fighter  Limited    21        \n";
+	cout << "J-Type 327     Light Combat   Unlimited  1         \n";
+	cout << "NX Cruiser     Medium Fighter Limited    18        \n";
+	cout << "N1 Starfighter Medium Fighter Unlimited  25        \n";
+	cout << "Royal Cruiser  Light Combat   Limited    4         \n";
+	return 0;
+}
