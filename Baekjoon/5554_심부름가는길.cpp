b85f4c9bd4f78d0c22c00f5c0cd289566013af7e
wh2per <93negaro@gmail.com>
2019-11-04 23:31:58 +0900

    5554_심부름가는길

---
+++ Baekjoon/5554_심부름가는길.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	freopen("input.txt", "r", stdin);
+
+	int a = 4;
+	int sum = 0;
+	while (a--) {
+		int n;
+		cin >> n;
+		sum += n;
+	}
+
+	cout << sum / 60 << "\n" << sum % 60;
+
+	return 0;
+}
