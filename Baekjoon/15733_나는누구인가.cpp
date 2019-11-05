22339049bfce21908cd36652530fb23da660a4b6
wh2per <93negaro@gmail.com>
2019-11-06 00:05:07 +0900

    15733_나는누구인가

---
+++ Baekjoon/15733_나는누구인가.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	//freopen("input.txt", "r", stdin);
+
+	cout << "I'm Sexy";
+
+	return 0;
+}
