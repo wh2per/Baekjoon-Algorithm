78d1bf5dede721b48924f3db42c6a8023727fc8b
wh2per <93negaro@gmail.com>
2019-11-06 00:06:15 +0900

    14645_와이버스부릉부릉

---
+++ Baekjoon/14645_와이버스부릉부릉.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	freopen("input.txt", "r", stdin);
+
+	int n, k;
+	cin >> n >> k;
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < k; ++j) {
+			int num;
+			cin >> num;
+		}
+	}
+
+	cout << "ºñ¿ÍÀÌ";
+
+	return 0;
+}
