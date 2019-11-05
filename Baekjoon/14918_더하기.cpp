ac6b25f50ff0081f19728bd35271bdfdb2f9ef29
wh2per <93negaro@gmail.com>
2019-11-06 00:06:08 +0900

    14918_더하기

---
+++ Baekjoon/14918_더하기.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	freopen("input.txt", "r", stdin);
+
+	int a, b;
+	cin >> a >> b;
+	cout << a + b;
+
+	return 0;
+}
