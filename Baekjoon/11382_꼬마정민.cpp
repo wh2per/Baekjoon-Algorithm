199bf91075aae58f1de939c290ea6fb313e2d40b
wh2per <93negaro@gmail.com>
2019-11-06 00:06:04 +0900

    11382_꼬마정민

---
+++ Baekjoon/11382_꼬마정민.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	long long a, b, c;
+	cin >> a >> b >> c;
+
+	cout << a + b + c;
+
+	return 0;
+}
