f051f90e31b947b0b27dbb010d517d6060bc8fe3
wh2per <93negaro@gmail.com>
2019-11-09 13:51:43 +0900

    2098_외판원순회

---
+++ Baekjoon/2098_외판원순회.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <queue>
+using namespace std;
+
+int w[16][16];
+int dp[16][1 << 16];
+int n;
+
+int tsp(int curr, int state) {
+	if (state == (1 << n) - 1) {
+		if (w[curr][0] == 0)
+			return INT_MAX-1;
+		else
+			return w[curr][0];
+	}
+
+	int& ret = dp[curr][state];
+	if (ret != 0)
+		return ret;
+
+	int m = INT_MAX-2;
+	for (int i = 1; i < n; ++i) {
+		if (((state & (1 << i)) == 0) && (w[curr][i] != 0))
+			m = min(m, tsp(i, (state | (1 << i))) + w[curr][i]);
+	}
+
+	return ret = m;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	freopen("input.txt", "r", stdin);
+
+	cin >> n;
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) 
+			cin >> w[i][j];
+	}
+
+	cout << tsp(0,1);
+
+	return 0;
+}
