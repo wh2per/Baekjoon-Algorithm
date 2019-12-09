ea9af4d2d14962692cef995411565dab4076ed4c
wh2per <93negaro@gmail.com>
2019-12-09 20:23:20 +0900

    14924_폰노이만과파리

---
+++ Baekjoon/14924_폰노이만과파리.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	freopen("input.txt", "r", stdin);
+
+	int s, t, d;
+	cin >> s >> t >> d;
+
+	cout << t * (d / (2 * s));
+
+	return 0;
+}
