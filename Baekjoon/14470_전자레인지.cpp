bb10aa2a74a264aec12b7a27ec93442fb55d6f91
wh2per <93negaro@gmail.com>
2019-12-21 17:12:39 +0900

    14470_전자레인지

---
+++ Baekjoon/14470_전자레인지.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	freopen("input.txt", "r", stdin);
+
+	int a, b, c, d, e;
+	cin >> b >> c >> d >> e;
+
+	if (a < 0) 
+		cout << -1 * a * c + d + e * b;
+	else if (a > 0)
+		cout << e * (b - a);
+	else\
+		cout << d + e * b;
+	return 0;
+}
