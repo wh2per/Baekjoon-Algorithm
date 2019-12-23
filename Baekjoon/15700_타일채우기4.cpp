d3cf924ef63fcbf8439ddbe854dd116c9af1297c
wh2per <93negaro@gmail.com>
2019-12-23 20:43:05 +0900

    15700_타일채우기4

---
+++ Baekjoon/15700_타일채우기4.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	//freopen("input.txt", "r", stdin);
+
+	unsigned long long n, m;
+	cin >> n >> m;
+	cout << n * m / 2;
+
+	return 0;
+}
