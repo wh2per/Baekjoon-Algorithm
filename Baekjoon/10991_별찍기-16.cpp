e46b198faa10133cb24a074cd410c4386a6eb7bc
wh2per <93negaro@gmail.com>
2019-12-24 01:36:38 +0900

    10991_별찍기-16

---
+++ Baekjoon/10991_별찍기-16.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	//freopen("input.txt", "r", stdin);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n-i-1; ++j)
+			cout << " ";
+		for (int j = 0; j < i + 1; ++j)
+			cout << "* ";
+		cout << endl;
+	}
+
+	return 0;
+}
