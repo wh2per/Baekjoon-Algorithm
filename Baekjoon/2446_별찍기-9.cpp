fa57e93c5f92bf1321d9ecbb21c6bbe65ec9099d
wh2per <93negaro@gmail.com>
2019-12-24 01:35:54 +0900

    2446_별찍기-9

---
+++ Baekjoon/2446_별찍기-9.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	//freopen("input.txt", "r", stdin);
+
+	int n;
+	cin >> n;
+
+	for (int i = n; i > 0; --i) {
+		for (int j = 0; j < n - i; ++j)
+			cout << " ";
+		for (int j = 0; j < 2 * i - 1; ++j)
+			cout << "*";
+		cout << endl;
+	}
+
+	for (int i = 2; i <= n; ++i) {
+		for (int j = 0; j < n - i; ++j)
+			cout << " ";
+		for (int j = 0; j < 2 * i - 1; ++j)
+			cout << "*";
+		cout << endl;
+	}
+
+	return 0;
+}
