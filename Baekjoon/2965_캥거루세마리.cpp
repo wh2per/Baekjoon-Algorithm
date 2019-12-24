8e7ce45953b43c5afb523f3c4191345fe6476ef9
wh2per <93negaro@gmail.com>
2019-12-25 01:01:52 +0900

    2965_캥거루세마리

---
+++ Baekjoon/2965_캥거루세마리.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	//freopen("input.txt", "r", stdin);
+
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << max(b - a, c - b) - 1;
+
+	return 0;
+}
