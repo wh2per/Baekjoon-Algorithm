f43d7d5d72c9ea6f16f902e0c8a989d762f3a387
wh2per <93negaro@gmail.com>
2020-01-07 20:10:47 +0900

    2754_학점계산

---
+++ Baekjoon/2754_학점계산.cpp
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <climits>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	//freopen("input.txt", "r", stdin);
+
+	string s;
+	cin >> s;
+
+	if (s[0] == 'A') {
+		if (s[1] == '+') {
+			cout << "4.3";
+		}
+		else if (s[1] == '0') {
+			cout << "4.0";
+		}
+		else {
+			cout << "3.7";
+		}
+	}
+	else if (s[0] == 'B') {
+		if (s[1] == '+') {
+			cout << "3.3";
+		}
+		else if (s[1] == '0') {
+			cout << "3.0";
+		}
+		else {
+			cout << "2.7";
+		}
+	}
+	else if (s[0] == 'C') {
+		if (s[1] == '+') {
+			cout << "2.3";
+		}
+		else if (s[1] == '0') {
+			cout << "2.0";
+		}
+		else {
+			cout << "1.7";
+		}
+	}
+	else if (s[0] == 'D') {
+		if (s[1] == '+') {
+			cout << "1.3";
+		}
+		else if (s[1] == '0') {
+			cout << "1.0";
+		}
+		else {
+			cout << "0.7";
+		}
+	}
+	else {
+		cout << "0.0";
+	}
+
+
+	return 0;
+}
