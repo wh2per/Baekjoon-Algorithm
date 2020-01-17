c647969593e1b0bb24bcdfba13a173192eac4d98
wh2per <93negaro@gmail.com>
2020-01-17 23:36:26 +0900

    5717_상근이의친구들

---
+++ Baekjoon/5717_상근이의친구들.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	//freopen("input.txt", "r", stdin);
+
+	while (1) {
+		int a, b;
+		cin >> a >> b;
+		if (a == 0 && b == 0)
+			break;
+
+		cout << a + b << endl;
+	}
+
+
+	return 0;
+}
