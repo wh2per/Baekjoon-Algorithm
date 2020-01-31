e92863ea4330d36ebde009427b381b2f2c1a5401
wh2per <93negaro@gmail.com>
2020-01-31 19:09:56 +0900

    14581_팬들에게둘러싸인홍준

---
+++ Baekjoon/14581_팬들에게둘러싸인홍준.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <climits>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	//freopen("input.txt", "r", stdin);
+
+	string s;
+	cin >> s;
+
+	cout << ":fan::fan::fan:" << endl;
+	cout << ":fan::" << s << "::fan:" << endl;
+	cout << ":fan::fan::fan:" << endl;
+
+	
+	return 0;
+}
