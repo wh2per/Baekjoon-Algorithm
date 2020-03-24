a8af0c5c8d187dfaf41c1427c1782f3c57671ac9
os.kwon <93negaro@gmail.com>
2020-03-24 18:18:55 +0900

    9251_LCS

--- Baekjoon/9251_LCS.cpp
+++ Baekjoon/9251_LCS.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int bj_9251() {
 	string s1, s2;
 	cin >> s1 >> s2;
-	vector<vector<int>> d(1000, vector<int>(1000, 0));
+	vector<vector<int>> d(1001, vector<int>(1001, 0));
 	for (int i = 1; i <= s1.length(); ++i) {
 		for (int j = 1; j <= s2.length(); ++j) {
 			if (s1[i - 1] == s2[j - 1])
