f3b67323870877c05cbe5799baccd53a90122d35
os.kwon <93negaro@gmail.com>
2020-05-02 16:47:50 +0900

    9085_더하기

---
+++ Baekjoon/9085_더하기.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n;
+		cin >> n;
+
+		int sum = 0;
+		while (n--) {
+			int a;
+			cin >> a;
+			sum += a;
+		}
+		cout << sum << "\n";
+	}
+
+	return 0;
+}
