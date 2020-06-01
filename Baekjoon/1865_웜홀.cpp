3d175e42dea3a711be5c3af075f120bbfe208349
os.kwon <93negaro@gmail.com>
2020-06-01 21:22:01 +0900

    1865_웜홀

--- Baekjoon/1865_웜홀.cpp
+++ Baekjoon/1865_웜홀.cpp
@@ -33,33 +33,27 @@ int main() {
 			int u, v, c;
 			cin >> u >> v >> c;
 
-			info[v].push_back({ u,-c });	// 반대로 저장!
+			info[u].push_back({ v,-c });
 		}
 
-		vector<int> dist(n + 1, INT_MAX);
-		dist[1] = 0;
+		vector<int> dist(n + 1, 987654321);
 		bool flag = false;
+		dist[0] = dist[1] = 0;
 
-		for (int i = 1; i <= n; ++i) {				// 간선은 다 사용할 필요없이 n-1보다만 많이 사용하면 됨	
-			for (int j = 1; j <= n; ++j) {			// 정점 1~N까지 돌려보기
-				for (int k = 0; k < info[j].size(); ++k) {		// 정점에서 갈 수 있는 곳
-					if (dist[info[j][k].first] == INT_MAX)		// 출발점이 아직 갱신된 곳이 아니라면 스킵
-						continue;
-					if (dist[j] > dist[info[j][k].first] + info[j][k].second) {		 // 값이 갱신 될때 
-						dist[j] = dist[info[j][k].first] + info[j][k].second;
-						if (i == n) {
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= n; ++j) {
+				for (int k = 0; k < info[j].size(); ++k) {
+					int to = info[j][k].first;
+					int cost = info[j][k].second;
+
+					if (dist[to] > dist[j] + cost) {
+						dist[to] = dist[j] + cost;
+						if (i == n)
 							flag = true;
-							break;
-						}
 					}
 				}
-				if (flag)
-					break;
 			}
-			if (flag)
-				break;
 		}
-
 		if (flag)
 			cout << "YES" << "\n";
 		else
