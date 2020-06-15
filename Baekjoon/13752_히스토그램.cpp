e8fe8c816bdf9e2001623383e399cc80bba681de
os.kwon <93negaro@gmail.com>
2020-06-16 01:51:33 +0900

    13752_히스토그램

---
+++ Baekjoon/13752_히스토그램.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	//freopen("input.txt", "r", stdin);
+
+	int n;
+	cin >> n;
+
+	while (n--) {
+		int num;
+		cin >> num;
+
+		while (num--)
+			cout << "=";
+		cout << "\n";
+	}
+
+	return 0;
+}
