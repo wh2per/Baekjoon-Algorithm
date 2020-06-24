d188d58c1a37dc0d38eaf1a74b80b2505c6c8ff8
os.kwon <93negaro@gmail.com>
2020-06-24 21:37:56 +0900

    11657_타임머신

--- Baekjoon/11657_타임머신.cpp
+++ Baekjoon/11657_타임머신.cpp
@@ -15,22 +15,23 @@ int main() {
 	int n, m;
 	cin >> n >> m;
 
-	vector<vector<pair<int, int>>> info(n + 1, vector<pair<int, int>>());
+	vector<vector<pair<int, long long>>> info(n + 1, vector<pair<int, long long>>());
 
 	for (int i = 0; i < m; ++i) {
-		int a, b, c;
+		int a, b;
+		long long c;
 		cin >> a >> b >> c;
 
-		info[b].push_back({a,c});		// 반대로! (나에게로 올 수 있는 정점, 그 때 비용)
+		info[b].push_back({ a,c });		// 반대로! (나에게로 올 수 있는 정점, 그 때 비용)
 	}
 
-	vector<int> dist(n + 1, INT_MAX);
+	vector<long long> dist(n + 1, LLONG_MAX);
 	dist[1] = 0;		// 시작점 초기화
 
 	for (int i = 1; i < n; ++i) {				// 사용할 간선 n-1만큼 돌려보기
 		for (int j = 1; j <= n; ++j) {			// 정점 1~N까지 돌려보기
 			for (int k = 0; k < info[j].size(); ++k) {		// 정점에서 갈 수 있는 곳
-				if (dist[info[j][k].first] == INT_MAX)		// 출발점이 아직 갱신된 곳이 아니라면 스킵
+				if (dist[info[j][k].first] == LLONG_MAX)		// 출발점이 아직 갱신된 곳이 아니라면 스킵
 					continue;
 				if (dist[j] > dist[info[j][k].first] + info[j][k].second) {		 // 값이 갱신 될때 
 					dist[j] = dist[info[j][k].first] + info[j][k].second;
@@ -38,12 +39,12 @@ int main() {
 			}
 		}
 	}
-	
+
 	// 음수 사이클이 있는 지 검사
 	bool flag = false;
 	for (int j = 1; j <= n; ++j) {			// 정점 1~N까지 돌려보기
 		for (int k = 0; k < info[j].size(); ++k) {		// 정점에서 갈 수 있는 곳
-			if (dist[info[j][k].first] == INT_MAX)		// 출발점이 아직 갱신된 곳이 아니라면 스킵
+			if (dist[info[j][k].first] == LLONG_MAX)		// 출발점이 아직 갱신된 곳이 아니라면 스킵
 				continue;
 			if (dist[j] > dist[info[j][k].first] + info[j][k].second) {		 // 값이 갱신 될때 
 				flag = true;
@@ -58,7 +59,7 @@ int main() {
 		cout << -1 << "\n";
 	else {
 		for (int i = 2; i <= n; ++i) {
-			if (dist[i] == INT_MAX)
+			if (dist[i] == LLONG_MAX)
 				dist[i] = -1;
 			cout << dist[i] << "\n";
 		}
