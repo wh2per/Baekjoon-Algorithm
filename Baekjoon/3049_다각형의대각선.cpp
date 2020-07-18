b5aeb0f934e3f10fea30eb77634c1b777b9caf55
os.kwon <93negaro@gmail.com>
2020-07-18 23:19:18 +0900

    3049_다각형의대각선

---
+++ Baekjoon/3049_다각형의대각선.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	//freopen("input.txt", "r", stdin);
+
+	int n;
+	cin >> n;
+	cout << n * (n - 1) * (n - 2) * (n - 3) / 24 << '\n';
+
+	return 0;
+}
