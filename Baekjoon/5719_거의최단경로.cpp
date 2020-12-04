8f68f89cba48c416942497f1bcf1ad6761440e1e
os.kwon <93negaro@gmail.com>
2020-12-04 18:07:34 +0900

    5719_거의최단경로

--- Baekjoon/5719_거의최단경로.cpp
+++ Baekjoon/5719_거의최단경로.cpp
@@ -4,22 +4,35 @@
 #include <queue>
 #include <algorithm>
 #include <climits>
-#include <string>
+#include <string.h>
 #include <cstring>
 using namespace std;
 
 // 다익스트라
 
-vector<pair<int, int>> info[500];
-vector<int> dist;
-vector<vector<int>> path[500];
+vector<pair<int, int>> info[501];
+int dist[501];
+vector<int> path[501];
+bool check[501];
+
+void dfs(int start, int end) {
+	for (int i = 0; i < info[start].size(); ++i) {
+		if (info[start][i].first == end) {
+			if (info[start][i].second == -1)
+				continue;
+			info[start][i].second = -1;
+			for (auto b : path[start])
+				dfs(b, start);
+		}
+	}
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	freopen("input.txt", "r", stdin);
+	//freopen("sample_input.txt", "r", stdin);
 
 	while (1) {
 		int n, m, s, d;
@@ -29,26 +42,23 @@ int main() {
 			break;
 
 		cin >> s >> d;
-		
-		for (int i = 0; i < 500; ++i) {		// info,path 초기화
+
+		for (int i = 0; i < 501; ++i) {		// info,path 초기화
 			info[i].clear();
 			path[i].clear();
 		}
-		dist.assign(n,INT_MAX);
+		fill(dist, dist + n, INT_MAX);
 
 		for (int i = 0; i < m; ++i) {
 			int u, v, p;
 			cin >> u >> v >> p;
-			info[u].push_back({v,p});
+			info[u].push_back({ v,p });
 		}
 
 		priority_queue<pair<int, int>> q;
-		
+
 		dist[s] = 0;
 		q.push({ 0,s });
-		vector<int> start;
-		start.push_back(s);
-		path[s].push_back(start);
 
 		while (!q.empty()) {
 			int cost = -q.top().first;
@@ -64,35 +74,23 @@ int main() {
 
 				if (dist[next] > dist[pos] + ncost) {		// 거리를 갱신해야 할 경우
 					dist[next] = dist[pos] + ncost;
-					q.push({-dist[next],next});
+					q.push({ -dist[next],next });
 
 					path[next].clear();
-					for (auto b : path[pos]) {			// 경로의 갯수
-						b.push_back(next);
-						path[next].push_back(b);
-					}
+					path[next].push_back(pos);
 				}
-				else if (dist[next] == dist[pos] + ncost) {		// path를 추가하자
-					for (auto b : path[pos]) {			// 경로의 갯수
-						b.push_back(next);
-						path[next].push_back(b);
-					}
-				}
+				else if (dist[next] == dist[pos] + ncost) 		// path를 추가하자
+					path[next].push_back(pos);
 			}
 		}
 
-		for (auto a : path[d]) {		// 삭제해야할 path들
-			for (int i = 0; i < a.size()-1; ++i) {
-				for (int j = 0; j < info[a[i]].size(); ++j) {
-					if (info[a[i]][j].first == a[i + 1])
-						info[a[i]][j].second = -1;			// 코스트를 -1로 바꿔버리기
-				}
-			}
-		}
+		memset(check, false, sizeof(check));
+		for (auto a : path[d])
+			dfs(a, d);
 
-		dist.assign(n, INT_MAX);
+		fill(dist, dist + n, INT_MAX);
 		priority_queue<pair<int, int>> nq;
-		
+
 		dist[s] = 0;
 		nq.push({ 0,s });
 		while (!nq.empty()) {
@@ -103,15 +101,18 @@ int main() {
 			if (pos == d)
 				break;
 
-			for (int i = 0; i < info[pos].size(); ++i) {
-				if (info[pos][i].second == -1)
+			if (cost > dist[d])
+				continue;
+
+			for (auto a : info[pos]) {
+				if (a.second == -1)
 					continue;
-				int ncost = info[pos][i].second;
-				int next = info[pos][i].first;
+				int ncost = a.second;
+				int next = a.first;
 
 				if (dist[next] > dist[pos] + ncost) {
 					dist[next] = dist[pos] + ncost;
-					nq.push({-dist[next], next});
+					nq.push({ -dist[next], next });
 				}
 			}
 		}
