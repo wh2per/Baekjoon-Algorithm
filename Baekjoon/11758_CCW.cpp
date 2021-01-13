8993b8e888362cc58ea93659e386fbfbb50dce39
os.kwon <93negaro@gmail.com>
2021-01-13 11:39:15 +0900

    11758_CCW

---
+++ Baekjoon/11758_CCW.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <queue>
+#include <string>
+#include <cmath>
+#include <string.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	//freopen("input.txt", "r", stdin);
+
+	// sin값이 양수:시계방향  0:일직선   음수:반시계방향
+
+	int x1, y1, x2, y2, x3, y3;
+	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+	if ((x1 * y2 - x2 * y1 + x2 * y3 - x3 * y2 + x3 * y1 - x1 * y3) / 2 > 0)
+		cout << '1' << '\n';
+	else if ((x1 * y2 - x2 * y1 + x2 * y3 - x3 * y2 + x3 * y1 - x1 * y3) / 2 < 0)
+		cout << "-1" << '\n';
+	else
+		cout << '0' << '\n';
+
+	return 0;
+}
